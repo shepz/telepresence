36db5bb51bb8c9e7f7e62d86244df5ba5bb9f26d
shepz <gveschambre@datawire.io>
2023-08-11 17:06:35 -0400

    Update release workflow to automate the release process
    Signed-off-by: shepz <gveschambre@datawire.io>

--- build-aux/main.mk
+++ build-aux/main.mk
@@ -259,7 +259,11 @@ prepare-release: generate wix
 	git add packaging/telepresence.wxs
 	git add packaging/bundle.wxs
 
-	git commit --signoff --message='Prepare $(TELEPRESENCE_VERSION)'
+	if git diff --cached --quiet; then \
+	  echo "No changes to commit."; \
+	else \
+	  git commit --signoff --message='Prepare $(TELEPRESENCE_VERSION)'; \
+	fi
 
 	git tag --annotate --message='$(TELEPRESENCE_VERSION)' $(TELEPRESENCE_VERSION)
 	git tag --annotate --message='$(TELEPRESENCE_VERSION)' rpc/$(TELEPRESENCE_VERSION)
